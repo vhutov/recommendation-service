1b4efb9779716dcf58453175d3676284e349e75f
vhutov <vladyslav.hutov@gmail.com>
2022-12-03 11:06:20 +0000

    add user service interface

--- index.js
+++ index.js
@@ -2,5 +2,60 @@ const config = require('./config')
 const { users } = require('./schema/dummy')
 
 const knex = require('knex')
+const { DateTime } = require('luxon')
 
+class UserService {
+    /**
+     * @type {knex.Knex}
+     */
+    #db
 
+    /**
+     * @param {knex.Knex} db
+     */
+    constructor(db) {
+        this.#db = db
+    }
+
+    /**
+     * Gets list of recently liked song ids
+     * @param {number} userId
+     * @param {{last_ts: ?number, limit: ?number}} options
+     * @returns {Promise<number[]>} list of song ids
+     */
+    getRecentLikedIds = async (userId, { last_ts = null, limit = null } = {}) => {}
+
+    /**
+     * Gets list of recently saved song ids
+     * @param {number} userId
+     * @param {{last_ts: ?number, limit: ?number}} options
+     * @returns {Promise<number[]>} list of song ids
+     */
+    getRecentSavedIds = async (userId, { last_ts = null, limit = null } = {}) => {}
+}
+
+async function main() {
+    /**
+     * @type {knex.Knex}
+     */
+    const db = knex(config.db)
+
+    const userService = new UserService(db)
+
+    const date = DateTime.now().minus({ days: 1 })
+    const options = { last_ts: date.toUnixInteger() }
+
+    const [liked, saved] = await Promise.all([
+        userService.getRecentLikedIds(users.Joe.id, options),
+        userService.getRecentSavedIds(users.Joe.id, options),
+    ])
+
+    console.log('Liked', liked)
+    console.log('Saved', saved)
+
+    console.log()
+
+    await db.destroy()
+}
+
+main()
