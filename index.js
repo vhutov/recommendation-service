f6b93e602aa6f752a62750cd9a473926b94afb79
vhutov <vladyslav.hutov@gmail.com>
2022-12-03 14:49:41 +0000

    add recommendations interface

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ const { DateTime } = require('luxon')
 const redis = require('redis')
 
 class UserService {
-    /**
-     * @type {knex.Knex}
-     */
+    /** @type {knex.Knex} */
     #db
 
     /**
@@ -72,9 +70,7 @@ class UserService {
 }
 
 class SimilarityService {
-    /**
-     * @type {redis.RedisClientType}
-     */
+    /** @type {redis.RedisClientType} */
     #redis
 
     /**
@@ -110,15 +106,13 @@ class SimilarityService {
 }
 
 class EnrichmentService {
-    /**
-     * @type {knex.Knex}
-     */
+    /** @type {knex.Knex} */
     #db
 
     /**
-     * @type {knex.Knex}
+     * @param {knex.Knex} db
      */
-    constructor (db) {
+    constructor(db) {
         this.#db = db
     }
 
@@ -134,11 +128,123 @@ class EnrichmentService {
             FROM 
                 songs
             WHERE 
-                id IN (${songIds.join(',')})`   
-        )
+                id IN (${songIds.join(',')})`)
 
         return rows
     }
+}
+
+class RecommendationService {
+    /** @type {UserService} */
+    #userService
+    /** @type {SimilarityService} */
+    #similarityService
+    /** @type {EnrichmentService} */
+    #enrichmentService
+
+    /**
+     * @param {UserService} userService
+     * @param {SimilarityService} similarityService
+     * @param {EnrichmentService} enrichmentService
+     */
+    constructor(userService, similarityService, enrichmentService) {
+        this.#userService = userService
+        this.#similarityService = similarityService
+        this.#enrichmentService = enrichmentService
+    }
+
+    /**
+     * Convert number (id) into User Entity
+     * @typedef {Object.<string, any>} Entity
+     * @param {number|number[]} input
+     * @returns {Promise<Entity[]>} input as user id
+     */
+    user = async (input) => {}
+
+    /**
+     * Copies property value to another property
+     * @typedef {Object.<string, any>} Entity
+     * @param {string} from param name
+     * @param {string} to param name
+     * @param {Entity|Entity[]} input
+     * @returns {Promise<Entity[]>} entities with new property set
+     */
+    set = (from, to) => async (input) => {}
+
+    /**
+     * Finds similar entities. Copies properties from parent to children.
+     * @typedef {Object.<string, any>} Entity
+     * @param {*} options see SimilarityService#getSimilar options
+     * @param {Entity|Entity[]} input
+     * @returns {Promise<Entity[]>} similar entities for every entity from input
+     */
+    similar = (options) => async (input) => {}
+
+    /**
+     * Enriches Song Entities with song data
+     * @typedef {Object.<string, any>} Entity
+     * @param {Entity|Entity[]} input
+     * @returns {Promise<Entity[]>} input entities, enriched with songs data
+     */
+    enrichSong = async (input) => {}
+
+    /**
+     * For User Entities fetches Song Entities. Copies over properties from parent to children.
+     * @typedef {Object.<string, any>} Entity
+     * @param {*} options see UserService#getRecentLikedIds
+     * @param {Entity|Entity[]} input
+     * @returns {Promise<Entity[]>} liked entities from input entities
+     */
+    liked =
+        (options = {}) =>
+        async (input) => {}
+
+    /**
+     * For User Entities fetches Song Entities. Copies over properties from parent to children.
+     * @typedef {Object.<string, any>} Entity
+     * @param {*} options see UserService#getRecentLikedIds
+     * @param {Entity|Entity[]} input
+     * @returns {Promise<Entity[]>} saved values from input entities
+     */
+    saved =
+        (options = {}) =>
+        async (input) => {}
+
+    /**
+     * Takes specified amount from input.
+     * @typedef {Object.<string, any>} Entity
+     * @param {number} limit
+     * @param {Entity|Entity[]} input
+     * @returns {Promise<Entity[]>} same as input but only [limit] elements
+     */
+    take = (limit) => async (input) => {}
+
+    /**
+     * Merges few flows into single flow
+     * @typedef {Object.<string, any>} Entity
+     * @param  {...Promise<Entity[]>} flows
+     * @returns {Promise<Entity[]>} multiple flows merged together
+     */
+    merge = async (...flows) => {}
+
+    /**
+     * Deduplicates entities by specified property
+     * @typedef {Object.<string, any>} Entity
+     * @param {string} by property name
+     * @param {Entity|Entity[]} input
+     * @returns {Promise<Entity[]>} same as input but deduplicated
+     */
+    dedupe = (by) => async (input) => {}
+
+    /**
+     * Tries to sort values in a way that puts similar items further away from
+     * each other
+     * @typedef {Object.<string, any>} Entity
+     * @param {string} by param name
+     * @param {Entity|Entity[]} input
+     * @returns {Promise<Entity[]>} same as input
+     */
+    diversify = (by) => async (input) => {}
 }
 
 async function main() {
@@ -153,25 +259,19 @@ async function main() {
     const similarityService = new SimilarityService(redisClient)
     const enrichmentService = new EnrichmentService(db)
 
+    const recs = new RecommendationService(userService, similarityService, enrichmentService)
+
     const date = DateTime.now().minus({ days: 2 })
     const options = { last_ts: date.toUnixInteger() }
 
-    const [liked, saved] = await Promise.all([
-        userService
-            .getRecentLikedIds(users.Joe.id, options)
-            .then((liked) => similarityService.getSimilar(liked, config.songsRecommendations))
-            .then((values) => Object.values(values).flat())
-            .then((ids) => enrichmentService.enrichSongData(ids)),
-        userService
-            .getRecentSavedIds(users.Joe.id, options)
-            .then((saved) => similarityService.getSimilar(saved, config.songsRecommendations))
-            .then((values) => Object.values(values).flat())
-            .then((ids) => enrichmentService.enrichSongData(ids)),
-    ])
+    const user = recs.user(users.Joe.id)
 
-    console.log('Liked', liked)
-    console.log('Saved', saved)
+    const recommendations = await recs
+        .merge(recs.liked(options)(user), recs.saved(options)(user))
+        .then(recs.similar(config.songsRecommendations))
+        .then(recs.enrichSongData)
 
+    console.log('Recommendations', recommendations)
 
     await db.destroy()
     await redisClient.disconnect()
